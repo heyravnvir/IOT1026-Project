46416eb156bfd69761b9af0f8a5a6dac5844b09f
Ranvir Singh <124471296+heyravnvir@users.noreply.github.com>
2023-06-18 06:16:11 +0000

    added test functions still need to add more

--- MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
+++ MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
@@ -1,4 +1,7 @@
-﻿using MinotaurLabyrinth;
+﻿﻿using System.Reflection;
+using MinotaurLabyrinth;
+using Moq;
+using Moq.Protected;
 
 namespace MinotaurLabyrinthTest
 {
@@ -10,5 +13,23 @@ namespace MinotaurLabyrinthTest
         {
             Assert.AreNotSame(1, 2);
         }
+
+
+[TestMethod]
+        public void Test_Activate_LostGame()
+        {
+            var mock = new Mock<Toxic>() { CallBase = true }; //need to set Callbase = true, then can call the override/virtual method
+            mock.Protected().Setup<int>("Dice3Dices").Returns(11);
+            mock.Protected().Setup<bool>("GetUserSelectOption").Returns(true);
+            var toxic = mock.Object;
+
+            (Map map, Hero hero) = LabyrinthCreator.InitializeMap(Size.Small);
+            toxic.Activate(hero, map);
+
+            mock.Protected().Verify("HandleLostGame", Times.Once(), new object[] { hero });
+            Assert.AreEqual(hero.IsPoisoned, true);
+        }
+
+
     }
 }
